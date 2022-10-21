3f47323a4d322e8983af63b11b342d8defd65028
cesarauguto316 <cesaraugusto316@protonmail.com>
2022-10-21 09:56:58 -0500

    🎨  refactoring

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,29 @@
 import express from 'express';
 import morgan from 'morgan';
 import { config } from 'dotenv';
+import { defaultErrorHandler } from './middlewares/defaultErrorHandler';
 
 
 config();
 export const app = express();
 
-// middlewares
+// 1. middlewares
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// 2. mounting routers
+app.use('/api/v1/ping', async (req, res, next) => {
+  try {
+    res.status(200).json({
+      message: 'pong'
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 3. defaultErrorHandler middleware
+app.use('/api/v1/', defaultErrorHandler);
