133dc6fc64353c479782a2b9be753fa1363a2f70
robertvelasco <robert.velasco.cel@gmail.com>
2022-11-09 22:08:55 -0500

    endpoint para Actualizar campos de un usuario Developers

--- __tests__/developersRouter.test.ts
+++ __tests__/developersRouter.test.ts
@@ -140,6 +140,64 @@ describe('post /developers/larnu/register', () => {
     });
   });
 
+  /*
+* Update user developer
+*/
+
+describe('patch /developers/larnu/register/id', () => {
+  const request = supertest.agent(app);
+
+  it('You must be able to update a user in the database.', async () => {
+    const payload = {
+      'name':'NombreUpdate',
+      'lastName':'ApellidoUpdate',
+      'age':20,
+      'city':'ciudadUpdate',
+      'country':'paisUpdate', 
+      'email':'correoUpdate@correo.com',
+      'active':true,
+      'skills':['skillUpdate1', 'skillUpdate2'],
+      'profession':'Desarrollador Update',
+      'description':'Actualizacion de datos',
+      'github':'https://www.linkedin.com/Update',  
+      'linkedin':'https://www.linkedin.com/Update',
+      'portfolio':'https://www.linkedin.com/Update'
+    };
+    const { body, status } = await request
+      .patch('/api/v1/developers/register/1')
+      .type('json')
+      .send(payload);
+
+    expect(status).toBe(200);
+    expect(body).toHaveProperty('message');
+  });
+
+  it('should throw 404 error if user id does not exist', async () => {
+    const payload = {
+      'name':'NombreUpdate',
+      'lastName':'ApellidoUpdate',
+      'age':20,
+      'city':'ciudadUpdate',
+      'country':'paisUpdate', 
+      'email':'correoUpdate@correo.com',
+      'active':true,
+      'skills':['skillUpdate1', 'skillUpdate2'],
+      'profession':'Desarrollador Update',
+      'description':'Actualizacion de datos',
+      'github':'https://www.linkedin.com/Update',  
+      'linkedin':'https://www.linkedin.com/Update',
+      'portfolio':'https://www.linkedin.com/Update'
+    };
+    const { body, status } = await request
+      .patch('/api/v1/developers/register/1000')
+      .type('json')
+      .send(payload);
+
+    expect(status).toBe(404);
+    expect(body).toHaveProperty('message');
+  });
+});
+
   
 });
 
--- src/developer/router.ts
+++ src/developer/router.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getAll, uploadImage, updateImage, deleteImage, registerDevelopers } from './controller';
+import { getAll, uploadImage, updateImage, deleteImage, registerDevelopers, updateDevelopers } from './controller';
 import processFileUpload from '../middlewares/processFileUpload';
 import { schemaValidator } from '../middlewares/schemaValidation';
 import { studentShema } from './student.shema';
@@ -18,3 +18,5 @@ developersRouter.route('/:id/photo')
 developersRouter.route('/register')
   .post(schemaValidator(studentShema), registerDevelopers);
 
+developersRouter.route('/register/:id')
+  .patch(schemaValidator(studentShema), updateDevelopers);
